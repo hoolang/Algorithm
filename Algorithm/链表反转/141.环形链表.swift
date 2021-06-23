da35e5788298ec1f32ef580162a65cbc7e2a0314
zmb <7283380@qq.com>
2021-06-23 22:47:53 +0800

    141 环形链表判断

--- Algorithm/链表反转/141.环形链表.swift
+++ Algorithm/链表反转/141.环形链表.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+class Solution_141 {
+    func hasCycle(_ head: ListNode?) -> Bool {
+        guard head != nil && head?.next != nil else {
+            return false
+        }
+        var slow:ListNode? = head
+        var fast:ListNode? = head?.next
+        while fast != nil && fast?.next != nil {
+            
+            slow = slow?.next
+            fast = fast?.next?.next
+            if fast?.val == slow?.val { // 暂时只通过val来判断是否相等
+                return true
+            }
+        }
+        
+        return false
+    }
+}
